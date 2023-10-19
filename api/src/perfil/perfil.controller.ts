72d5a5cf85a221f8bd39e65f8c67b87aea0818f5
Leonardo Minora <leonardo.minora@gmail.com>
2023-10-19 14:24:15 -0300

    adicionado rota GET /:apelido/perfil sem imagem

--- api/src/perfil/perfil.controller.ts
+++ api/src/perfil/perfil.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { PerfilService } from './perfil.service';
 
-@Controller('perfil')
-export class PerfilController {}
+@Controller(':apelido/perfil')
+export class PerfilController {
+  constructor(private readonly servico: PerfilService) {}
+
+  @Get()
+  recuperar(@Param() parametro: any) {
+    const perfil = this.servico.recuperarPorApelido(parametro.apelido);
+    if (perfil) {
+      return {
+        estado: 'ok',
+        ...perfil,
+      };
+    }
+    return {
+      estado: 'nok',
+      mensagem: `perfil do usuário ${parametro.apelido} não encontrado`,
+    };
+  }
+}
--- api/src/perfil/perfil.service.ts
+++ api/src/perfil/perfil.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { PerfilDTO } from './dto/PerfilDTO';
 
+const perfis: PerfilDTO[] = [
+  {
+    id: 1,
+    nome_social: 'alunos',
+    descricao: 'conta única dos alunos de infoweb',
+  },
+];
 @Injectable()
-export class PerfilService {}
+export class PerfilService {
+  recuperarPorApelido(apelido: string) {
+    const perfil = perfis.filter((perfil) => perfil.nome_social === apelido);
+    return perfil[0] ?? undefined;
+  }
+}
