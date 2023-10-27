bc37a5eed8764b90dee86a0ba487354d79362c15
Leonardo Minora <leonardo.minora@gmail.com>
2023-10-27 20:30:52 -0300

    web - adicionado html exemplo

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,6 +2,24 @@ const App = () => {
 	return (
 		<>
 			<h1>projeto react criado</h1>
+			<form>
+				<div>
+					<label htmlFor="apelido">Apelido</label>
+					<input type="text" name="apelido" />
+				</div>
+				<div>
+					<label htmlFor="descricao">Eu sou...</label>
+					<input type="text" name="descricao" />
+				</div>
+				<div>
+					<label htmlFor="upload">Imagem do perfil</label>
+					<input type="file" name="upload" accept="image/*" />
+				</div>
+				<div>
+					<label htmlFor="imagem">Imagem do perfil</label>
+					<img name="imagem" alt="imagem do perfil"/>
+				</div>
+			</form>
 		</>
 	);
 };
